05f65a6f7dfb34c561a22b39d7bcd41dd49ad9b9
bin <bin6160@126.com>
2017-09-17 15:05:26 +0800

    use DialogFragment replace AddRecordActivity

---
+++ app/src/main/java/com/dev/bins/daily/main/AddDialog.kt
@@ -0,0 +1,67 @@
+package com.dev.bins.daily.main
+
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.text.TextUtils
+import android.view.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import butterknife.bindView
+import com.dev.bins.daily.R
+import com.dev.bins.daily.database.Record
+
+/**
+ * Created by bin on 17/09/2017.
+ */
+class AddDialog : DialogFragment() {
+
+
+    val mBtnSave: Button by bindView(R.id.btn_save)
+    val mBtnCancel: Button by bindView(R.id.btn_cancel)
+    val mEtContent: EditText by bindView(R.id.et_content)
+
+
+    override fun onResume() {
+        super.onResume()
+        var params = dialog.window.attributes
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT
+        params.height = ViewGroup.LayoutParams.MATCH_PARENT
+        dialog.window.attributes = params
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.window.setBackgroundDrawableResource(android.R.color.transparent)
+        dialog.setCanceledOnTouchOutside(false)
+        dialog.window.setGravity(Gravity.BOTTOM)
+        return inflater!!.inflate(R.layout.dialog_add_record, container)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view!!.post {
+            val animator = ViewAnimationUtils.createCircularReveal(view, view!!.width, view.height, 0f, view.height.toFloat())
+            animator.duration = 200
+            animator.start()
+        }
+        mBtnSave.setOnClickListener {
+            var content = mEtContent.text.toString().trim()
+            if (TextUtils.isEmpty(content)) {
+                Toast.makeText(activity, getText(R.string.content_empty), Toast.LENGTH_SHORT)
+                return@setOnClickListener
+            }
+            var record = Record()
+            record.content = content
+            record.save()
+            dismiss()
+        }
+        mBtnCancel.setOnClickListener {
+            dismiss()
+        }
+
+
+    }
+
+
+}
--- app/src/main/java/com/dev/bins/daily/main/MainActivity.kt
+++ app/src/main/java/com/dev/bins/daily/main/MainActivity.kt
@@ -1,16 +1,14 @@
 package com.dev.bins.daily.main
 
-import android.content.Intent
+
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
-import android.support.v4.app.ActivityOptionsCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import butterknife.bindView
 import com.dev.bins.daily.R
 import com.dev.bins.daily.adapter.DailyAdapter
-import com.dev.bins.daily.addrecord.AddRecordActivity
 import com.dev.bins.daily.database.Record
 import com.raizlabs.android.dbflow.sql.language.SQLite
 
@@ -24,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     var adapter: DailyAdapter? = null
     val datas = ArrayList<Record>()
 
-
+    val addDialog = AddDialog()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -39,8 +37,7 @@ class MainActivity : AppCompatActivity() {
         recycleView.layoutManager = LinearLayoutManager(this)
         recycleView.adapter = adapter
         fab.setOnClickListener {
-            val intent = Intent(applicationContext, AddRecordActivity::class.java)
-            startActivity(intent, ActivityOptionsCompat.makeSceneTransitionAnimation(this).toBundle())
+            addDialog.show(supportFragmentManager, "add")
         }
     }
 
