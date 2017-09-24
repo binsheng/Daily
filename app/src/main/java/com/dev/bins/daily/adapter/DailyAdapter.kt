be0e612bd908f3ee19424fcf09b76b0ee9c654c9
bin <bin6160@126.com>
2017-09-24 14:17:15 +0800

    format date

--- app/src/main/java/com/dev/bins/daily/adapter/DailyAdapter.kt
+++ app/src/main/java/com/dev/bins/daily/adapter/DailyAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import com.dev.bins.daily.R
 import com.dev.bins.daily.database.Record
 import kotlinx.android.synthetic.main.daily_item.view.*
+import java.text.SimpleDateFormat
 
 /**
  * Created by bin on 16/09/2017.
@@ -14,7 +15,7 @@ import kotlinx.android.synthetic.main.daily_item.view.*
 class DailyAdapter(datas: ArrayList<Record>) : RecyclerView.Adapter<DailyAdapter.Holder>() {
 
     var datas: ArrayList<Record>
-
+    val sdf = SimpleDateFormat("HH:mm")
 
     init {
         this.datas = datas
@@ -22,7 +23,7 @@ class DailyAdapter(datas: ArrayList<Record>) : RecyclerView.Adapter<DailyAdapter
 
 
     override fun onBindViewHolder(holder: Holder?, position: Int) {
-        holder?.bindData(datas[position])
+        holder?.bindData(datas[position], sdf)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): Holder {
@@ -40,10 +41,14 @@ class DailyAdapter(datas: ArrayList<Record>) : RecyclerView.Adapter<DailyAdapter
         val tvStartTime = itemView.tvStartTime
         val tvEndTime = itemView.tvEndTime
 
-        fun bindData(record: Record) {
+        fun bindData(record: Record, sdf: SimpleDateFormat) {
             tvContent.text = record.content
-            tvStartTime.text = record.startDate?.toString() ?: "-"
-            tvEndTime.text = record.endDate?.toString() ?: itemView.context.getString(R.string.doing)
+            tvStartTime.text = sdf.format(record.startDate)
+            if (null != record.endDate) {
+                tvEndTime.text = sdf.format(record.endDate)
+            } else {
+                tvEndTime.text = itemView.context.getString(R.string.doing)
+            }
         }
 
     }
--- app/src/main/java/com/dev/bins/daily/main/AddDialog.kt
+++ app/src/main/java/com/dev/bins/daily/main/AddDialog.kt
@@ -5,7 +5,6 @@ import android.app.TimePickerDialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
 import android.text.Editable
-import android.text.TextUtils
 import android.text.TextWatcher
 import android.view.*
 import android.widget.Toast
